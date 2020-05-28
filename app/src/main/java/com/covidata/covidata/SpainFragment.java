62418268135e1e64f542d7888141a37afcb219e6
Sara <sara.mulas89@gmail.com>
2020-05-28 19:38:01 +0200

    Added fragments

--- app/src/main/java/com/covidata/covidata/SpainFragment.java
+++ app/src/main/java/com/covidata/covidata/SpainFragment.java
@@ -4,9 +4,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SpainFragment extends Fragment {
@@ -18,6 +35,47 @@ public class SpainFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_spain, container, false);
 
         return view;
+    }
+
+    private void hacerPeticion() {
+
+        String url = "";
+
+        StringRequest postRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        ParseoJSON p=new ParseoJSON();
+                        Log.d("Response", response);
+
+                        try {
+                            DatoGlobal datoGlobal =p.parsearJSON(response);//Este response es el String JSON que le pasamos al metodo
+                            //crearGrafico(datoGlobal);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+
+            @Override
+            public Map<String, String> getHeaders(){
+                Map<String, String>  params = new HashMap<>();
+                params.put("Accept","application/xml");
+                return params;
+            }
+
+        };
+        Volley.newRequestQueue(getActivity().getApplicationContext()).add(postRequest);
     }
 
 
