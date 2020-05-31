e25610a4a50b68afb4563bba61475da1e597cab0
Sara <sara.mulas89@gmail.com>
2020-06-01 00:47:09 +0200

    Added CCAA

--- app/src/main/java/com/covidata/covidata/ComunidadAutonomaFragment.java
+++ app/src/main/java/com/covidata/covidata/ComunidadAutonomaFragment.java
@@ -3,6 +3,7 @@ package com.covidata.covidata;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,156 +25,57 @@ import com.anychart.enums.MarkerType;
 import com.anychart.enums.TooltipPositionMode;
 import com.anychart.graphics.vector.Stroke;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class ComunidadAutonomaFragment extends Fragment {
 
     View view;
-    ImageView imagen_pais;
+    RecyclerView recyclerView;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_comunidad_autonoma, container, false);
 
-        imagen_pais=view.findViewById(R.id.imagen_ccaa );
-        imagen_pais.setImageResource(R.drawable.galicia);
-
-        crearGrafico();
+        recyclerView = view.findViewById(R.id.recyclerview);
+        ArrayList<String> lista=crearListaCCAA();
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(lista);
+        recyclerView.setAdapter((recyclerAdapter));
 
         return view;
     }
 
-    public void crearGrafico(){
-
-        AnyChartView anyChartView = view.findViewById(R.id.grafico1);
-        APIlib.getInstance().setActiveAnyChartView(anyChartView);
-
-        Pie pie = AnyChart.pie();
-        String colores[] = {"#BF8A26", "#591E3A", "#30728C"};
-        pie.bounds(65, -10, "270px", "280px");
-
-        List<DataEntry> data = new ArrayList<>();
-
-        data.add(new ValueDataEntry("Confirmados", 1));
-        data.add(new ValueDataEntry("Fallecidos", 2));
-        data.add(new ValueDataEntry("Recuperados", 3));
-
-        pie.data(data);
-
-        pie.labels().enabled(false);
-        pie.selected().explode("0%");
-        pie.palette(colores);
-        pie.tooltip().enabled(false);
-
-
-        pie.legend()
-                .enabled(false);
-
-        anyChartView.setChart(pie);
-
-        AnyChartView anyChartView1 = view.findViewById(R.id.grafico2);
-        APIlib.getInstance().setActiveAnyChartView(anyChartView1);
-
-        Cartesian cartesian = AnyChart.line();
-        cartesian.height("260px");
-        cartesian.width("390px");
-
-        cartesian.animation(true);
-
-        cartesian.padding(10d, 20d, 5d, 20d);
-
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-
-        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
-
-        List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new CustomDataEntry("1986", 3.6, 2.3, 2.8));
-        seriesData.add(new CustomDataEntry("1987", 7.1, 4.0, 4.1));
-        seriesData.add(new CustomDataEntry("1988", 8.5, 6.2, 5.1));
-        seriesData.add(new CustomDataEntry("1989", 9.2, 11.8, 6.5));
-        seriesData.add(new CustomDataEntry("1990", 10.1, 13.0, 12.5));
-        seriesData.add(new CustomDataEntry("1991", 11.6, 13.9, 18.0));
-        seriesData.add(new CustomDataEntry("1992", 16.4, 18.0, 21.0));
-        seriesData.add(new CustomDataEntry("1993", 18.0, 23.3, 20.3));
-        seriesData.add(new CustomDataEntry("1994", 13.2, 24.7, 19.2));
-        seriesData.add(new CustomDataEntry("1995", 12.0, 18.0, 14.4));
-        seriesData.add(new CustomDataEntry("1996", 3.2, 15.1, 9.2));
-        seriesData.add(new CustomDataEntry("1997", 4.1, 11.3, 5.9));
-        seriesData.add(new CustomDataEntry("1998", 6.3, 14.2, 5.2));
-        seriesData.add(new CustomDataEntry("1999", 9.4, 13.7, 4.7));
-        seriesData.add(new CustomDataEntry("2000", 11.5, 9.9, 4.2));
-        seriesData.add(new CustomDataEntry("2001", 13.5, 12.1, 1.2));
-        seriesData.add(new CustomDataEntry("2002", 14.8, 13.5, 5.4));
-        seriesData.add(new CustomDataEntry("2003", 16.6, 15.1, 6.3));
-        seriesData.add(new CustomDataEntry("2004", 18.1, 17.9, 8.9));
-        seriesData.add(new CustomDataEntry("2005", 17.0, 18.9, 10.1));
-        seriesData.add(new CustomDataEntry("2006", 16.6, 20.3, 11.5));
-        seriesData.add(new CustomDataEntry("2007", 14.1, 20.7, 12.2));
-        seriesData.add(new CustomDataEntry("2008", 15.7, 21.6, 10));
-        seriesData.add(new CustomDataEntry("2009", 12.0, 22.5, 8.9));
-
-        Set set = Set.instantiate();
-        set.data(seriesData);
-        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
-        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
-        Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
-
-        Line series1 = cartesian.line(series1Mapping);
-        series1.name("Confirmados");
-        series1.hovered().markers().enabled(true);
-        series1.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series1.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        Line series2 = cartesian.line(series2Mapping);
-        series2.name("Fallecidos");
-        series2.hovered().markers().enabled(true);
-        series2.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series2.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        Line series3 = cartesian.line(series3Mapping);
-        series3.name("Recuperados");
-        series3.hovered().markers().enabled(true);
-        series3.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series3.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        cartesian.legend().enabled(true);
-        cartesian.legend().fontSize(13d);
-        cartesian.legend().padding(0d, 0d, 10d, 0d);
-        cartesian.legend().itemsLayout("horizontalExpandable").margin(3);
-
-        anyChartView1.setChart(cartesian);
-
+    public static ArrayList<String> crearListaCCAA(){
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("Andalucía");
+        lista.add("Aragón");
+        lista.add("Asturias");
+        lista.add("Cantabria");
+        lista.add("Castilla-La Mancha");
+        lista.add("Castilla León");
+        lista.add("Cataluña");
+        lista.add("Extremadura");
+        lista.add("Galicia");
+        lista.add("Islas Baleares");
+        lista.add("Canarias");
+        lista.add("La Rioja");
+        lista.add("Comunidad de Madrid");
+        lista.add("Murcia");
+        lista.add("Navarra");
+        lista.add("Pais Vasco");
+        lista.add("Comunidad Valenciana");
+        lista.add("Ceuta");
+        lista.add("Melilla");
+        return lista;
     }
-    private class CustomDataEntry extends ValueDataEntry {
 
-        CustomDataEntry(String x, Number value, Number value2, Number value3) {
-            super(x, value);
-            setValue("value2", value2);
-            setValue("value3", value3);
-        }
 
-    }
+
+
 
 
 
