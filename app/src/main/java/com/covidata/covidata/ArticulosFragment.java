32416397080d7ed349ad2eebf2b9bf7c821b8a3f
Sara <sara.mulas89@gmail.com>
2020-06-08 16:44:57 +0200

    Added articles

--- app/src/main/java/com/covidata/covidata/ArticulosFragment.java
+++ app/src/main/java/com/covidata/covidata/ArticulosFragment.java
@@ -32,6 +32,7 @@ public class ArticulosFragment extends Fragment {
 
     View view;
     RecyclerView recyclerView;
+    WebView webView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,41 +41,9 @@ public class ArticulosFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerview);
         ArrayList<Articulo> lista=crearLista();
 
-        AssetManager assetManager = getContext().getAssets();
-
-        InputStream in = null;
-        OutputStream out = null;
-        File file = new File(getContext().getFilesDir(), "uno.pdf");
-        try {
-            in = assetManager.open("uno.pdf");
-            out = new BufferedOutputStream(new FileOutputStream(file));
-
-            copyFile(in, out);
-            in.close();
-            in = null;
-            out.flush();
-            out.close();
-            out = null;
-        } catch (Exception e) {
-            Log.e("tag", e.getMessage());
-        }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(
-                Uri.parse("file://" + getContext().getFilesDir() + "/uno.pdf"),
-                "application/pdf");
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
-
-        Intent intent1 = Intent.createChooser(intent, "Open With");
-        try {
-            startActivity(intent1);
-        } catch (ActivityNotFoundException e) {
-            // Instruct the user to install a PDF reader here, or something
-        }
-
-        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-
+        webView = view.findViewById(R.id.web);
+        webView.loadUrl(lista.get(0).getPdf());
+        webView.getSettings().setJavaScriptEnabled(true);
 
         return view;
     }
@@ -82,15 +51,9 @@ public class ArticulosFragment extends Fragment {
     public static ArrayList<Articulo> crearLista(){
         ArrayList<Articulo> lista=new ArrayList<>();
         lista.add(new Articulo("Articulo 1",R.drawable.canada,
-                "https://reader.elsevier.com/reader/sd/pii/S1007570420301350?token=BA387DDAC46A34ABF52C177D0FD69F78CDA6595057DA7948E4D5ED7C826C79C8136E096390805F215C2865808986FAAD"));
+                "https://www.sciencedirect.com/science/article/pii/S1007570420301350"));
         return lista;
     }
 
-    private void copyFile(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int read;
-        while ((read = in.read(buffer)) != -1) {
-            out.write(buffer, 0, read);
-        }
-    }
+
 }
